11933bce2ef5e1889374a91b120b97bf44f9c9d3
Satyaa <satyaaiosdev@gmail.com>
2021-05-01 22:02:55 +0530

    Initial Commit

---
+++ Bubble/ViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  Bubble
+//
+//  Created by Satyaa on 28/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
+@IBDesignable
+class PaddingLabel: UILabel {
+    
+    override func layoutSubviews() {
+        let constraintRect = CGSize(width: self.frame.width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.text!.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: self.font], context: nil)
+            self.frame.size = CGSize(width: ceil(boundingBox.width), height: ceil(boundingBox.height))
+
+            let bubbleSize = CGSize(width: self.frame.width + 28, height: self.frame.height + 20)
+
+            let width = bubbleSize.width
+            let height = bubbleSize.height
+
+            let bezierPath = UIBezierPath()
+            bezierPath.move(to: CGPoint(x: width - 22, y: height))
+            bezierPath.addLine(to: CGPoint(x: 17, y: height))
+            bezierPath.addCurve(to: CGPoint(x: 0, y: height - 17), controlPoint1: CGPoint(x: 7.61, y: height), controlPoint2: CGPoint(x: 0, y: height - 7.61))
+            bezierPath.addLine(to: CGPoint(x: 0, y: 17))
+            bezierPath.addCurve(to: CGPoint(x: 17, y: 0), controlPoint1: CGPoint(x: 0, y: 7.61), controlPoint2: CGPoint(x: 7.61, y: 0))
+            bezierPath.addLine(to: CGPoint(x: width - 21, y: 0))
+            bezierPath.addCurve(to: CGPoint(x: width - 4, y: 17), controlPoint1: CGPoint(x: width - 11.61, y: 0), controlPoint2: CGPoint(x: width - 4, y: 7.61))
+            bezierPath.addLine(to: CGPoint(x: width - 4, y: height - 11))
+            bezierPath.addCurve(to: CGPoint(x: width, y: height), controlPoint1: CGPoint(x: width - 4, y: height - 1), controlPoint2: CGPoint(x: width, y: height))
+            bezierPath.addLine(to: CGPoint(x: width + 0.05, y: height - 0.01))
+            bezierPath.addCurve(to: CGPoint(x: width - 11.04, y: height - 4.04), controlPoint1: CGPoint(x: width - 4.07, y: height + 0.43), controlPoint2: CGPoint(x: width - 8.16, y: height - 1.06))
+            bezierPath.addCurve(to: CGPoint(x: width - 22, y: height), controlPoint1: CGPoint(x: width - 16, y: height), controlPoint2: CGPoint(x: width - 19, y: height))
+            bezierPath.close()
+
+            let outgoingMessageLayer = CAShapeLayer()
+            outgoingMessageLayer.path = bezierPath.cgPath
+            outgoingMessageLayer.frame = CGRect(x: self.frame.width/2 - width/2, y: self.frame.height/2 - height/2,width: width, height: height)
+            outgoingMessageLayer.fillColor = UIColor(red: 0.09, green: 0.54, blue: 1, alpha: 1).cgColor
+        self.layer.masksToBounds = true
+        self.layer.addSublayer(outgoingMessageLayer)
+       // self.layer.mask = outgoingMessageLayer
+    }
+    var textEdgeInsets = UIEdgeInsets.zero {
+        didSet { invalidateIntrinsicContentSize() }
+    }
+    
+    open override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        let insetRect = bounds.inset(by: textEdgeInsets)
+        let textRect = super.textRect(forBounds: insetRect, limitedToNumberOfLines: numberOfLines)
+        let invertedInsets = UIEdgeInsets(top: -textEdgeInsets.top, left: -textEdgeInsets.left, bottom: -textEdgeInsets.bottom, right: -textEdgeInsets.right)
+        return textRect.inset(by: invertedInsets)
+    }
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: textEdgeInsets))
+    }
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        set { self.layer.cornerRadius = newValue }
+        get { return 4 }
+    }
+    @IBInspectable
+    var paddingLeft: CGFloat {
+        set { textEdgeInsets.left = newValue }
+        get { return textEdgeInsets.left }
+    }
+    
+    @IBInspectable
+    var paddingRight: CGFloat {
+        set { textEdgeInsets.right = newValue }
+        get { return textEdgeInsets.right }
+    }
+    
+    @IBInspectable
+    var paddingTop: CGFloat {
+        set { textEdgeInsets.top = newValue }
+        get { return textEdgeInsets.top }
+    }
+    
+    @IBInspectable
+    var paddingBottom: CGFloat {
+        set { textEdgeInsets.bottom = newValue }
+        get { return textEdgeInsets.bottom }
+    }
+}
